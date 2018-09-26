1a29dff370cbf43900061479076c2772d733ab9a
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-09-26 14:21:49 +0900

    Add files via upload

--- greatestCommonMeasure.js
+++ greatestCommonMeasure.js
@@ -1,58 +1,81 @@
+//망해써ㅓㅓㅓㅋㅋ
+
 //최대공약수 만들기
+
+//구도 쪼개기
+//공약수란 무엇일까?
+//최대공약수는?
 
 // var strNum = prompt("두 수를 띄어서 입력해봐").split(" ");
 var strNum = "16 36";
 var strSplitNum = strNum.split(" ");
-var userNumberArr = strSplitNum.map(strNumber => parseInt(strNumber, 10));
-console.log("유저넘버 배열 : " + userNum);
-var primeNumberArr = [];
+var userNumbers = strSplitNum.map(strNumber => parseInt(strNumber, 10));
+console.log("유저넘버 배열 : " + userNumbers);
 
-if (userNumberArr[0] < userNumberArr[1]) {
-    for (i = 2; i <= userNumberArr[1]; i++) {
-        if (userNumberArr[1] % i === 0) primeNumberArr.push(i);
-    }
-}
-
-if (userNumberArr[1] < userNumberArr[0]) {
-    for (i = 2; i <= userNumberArr[0]; i++) {
-        if (userNumberArr[0] % i === 0) primeNumberArr.push(i);
-    }
-}
-
-//첫번째 배열의 수의 약수배열
-var len = primeNumberArr.length;
-var commonFactorArr1 = [];
-for (i = 0; i < len; i++) {
-    var dividedNumber = userNumberArr[0] / primeNumberArr[i]
-    if (dividedNumber !== 0) {
-        commonFactorArr.push(i);
-        if (dividedNumber /= primeNumberArr[i] !== 0) {
-            commonFactorArr.push(i);
-            if ()...
+//두 수 중 큰 수의 소수배열
+function getPrimeNumbers(userNumbers) {
+    var primeNumbers ;
+    if (userNumbers[0] > userNumbers[1]) {
+        for (i = 2; i <= userNumbers[1]; i++) {
+            if (userNumbers[1] % i === 0) primeNumbers.push(i);
         }
     }
-}
-//두번째 배열의 수의 약수배열
-var len = primeNumberArr.length;
-var commonFactorArr2 = [];
-for (i = 0; i < len; i++) {
-    var dividedNumber = userNumberArr[1] / primeNumberArr[i]
-    if (dividedNumber !== 0) {
-        commonFactorArr.push(i);
-        if (dividedNumber /= primeNumberArr[i] !== 0) {
-            commonFactorArr.push(i);
-            if ()...
+    if (userNumbers[1] > userNumbers[0]) {
+        for (i = 2; i <= userNumbers[0]; i++) {
+            if (userNumbers[0] % i === 0) primeNumbers.push(i);
         }
     }
+    return primeNumbers;
+}
+console.log(getPrimeNumbers(userNumbers));
+
+//약수배열 만들기
+function getDivisorNumbers1(userNumbers, userNumbersArr) {
+    console.log(userNumbers)
+    console.log(userNumbersArr)
+    var divisorNumbers1 = [];
+    var len = userNumbersArr.length;
+    for (i = 0; i < len; i++) {
+        if (userNumbers[1] / userNumbersArr[i] == 0) {
+            divisorNumbers1.push(i)
+        }
+    }
+    return divisorNumbers1;
+}
+function getDivisorNumbers2(userNumbers, userNumbersArr) {
+    var divisorNumbers2 = [];
+    var len = userNumbersArr.length;
+    for (i = 0; i < len; i++) {
+        if (userNumbers[0] / userNumbersArr[i] == 0) {
+            divisorNumbers1.push(i)
+        }
+    }
+    return divisorNumbers2;
 }
 
-var len1 = commonFactorArr1.length;
-var len2 = commonFactorArr2.length;
-var commonMeasureArr = [];
-for (i = 0; i < len1; i++) {
-    for (j = 0; j < len2; j++) {
-        if (commonFactorArr1[i] == commonFactorArr2[j]){
-            commonMeasureArr.push(commonFactorArr1[i]);
-        }   
+//공약수배열 만들기
+function getCommonFactors(divisorNumbers1, divisorNumbers2) {
+    var len1 = divisorNumbers1.length;
+    var len2 = divisorNumbers2.length;
+    var commonMeasures = [];
+    for (i = 0; i < len1; i++) {
+        for (j = 0; j < len2; j++) {
+            if (divisorNumbers1[i] == divisorNumbers2[j]) {
+                commonMeasures.push(divisorNumbers1[i]);
+            }
+        }
     }
+    console.log(commonMeasures);
+    console.log(commonMeasures.slice(-1)); //최대공약수!!!도출
 }
+
+function main() {
+    var userNumbersArr = getPrimeNumbers(userNumbers);
+    console.log(userNumbersArr);
+    console.log(userNumbers);
+    var divisorNumbers1 = getDivisorNumbers1(userNumbersArr, userNumbers);
+    console.log(getDivisorNumbers1(userNumbers, userNumbersArr));
+    var divisorNumbers2 = getDivisorNumbers1(userNumbersArr, userNumbers);
+    console.log(getDivisorNumbers2(userNumbers, userNumbersArr));
+}
+main();
