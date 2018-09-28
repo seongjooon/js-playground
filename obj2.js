301669e8eb6873f28ea33dca37c835fd41dfe97b
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-09-28 19:37:16 +0900

    Add files via upload

---
+++ obj2.js
@@ -0,0 +1,81 @@
+//type이 sk인, name으로 구성된 배열만 출력해본다.
+[{
+	"id": 1,
+	"name": "Yong",
+	"phone": "010-0000-0000",
+	"type": "sk",
+	"childnode": [{
+		"id": 11,
+		"name": "echo",
+		"phone": "010-0000-1111",
+		"type": "kt",
+		"childnode": [{
+				"id": 115,
+				// "name": "hary",
+				"phone": "211-1111-0000",
+				// "type": "sk",
+				"childnode": [{
+					"id": 1159,
+					"name": "pobi",
+					"phone": "010-444-000",
+					"type": "kt",
+					"childnode": [{
+							"id": 11592,
+							"name": "cherry",
+							"phone": "111-222-0000",
+							"type": "lg",
+							"childnode": []
+						},
+						{
+							"id": 11595,
+							// "name": "solvin",
+							"phone": "010-000-3333",
+							// "type": "sk",
+							"childnode": []
+						}
+					]
+				}]
+			},
+			{
+				"id": 116,
+				"name": "kim",
+				"phone": "444-111-0200",
+				"type": "kt",
+				"childnode": [{
+					"id": 1168,
+					// "name": "hani",
+					"phone": "010-222-0000",
+					// "type": "sk",
+					"childnode": [{
+						"id": 11689,
+						"name": "ho",
+						"phone": "010-000-0000",
+						"type": "kt",
+						"childnode": [{
+								"id": 116890,
+								"name": "wonsuk",
+								"phone": "010-000-0000",
+								"type": "kt",
+								"childnode": []
+							},
+							{
+								"id": 1168901,
+								// "name": "chulsu",
+								"phone": "010-0000-0000",
+								// "type": "sk",
+								"childnode": []
+							}
+						]
+					}]
+				}]
+			},
+			{
+				"id": 117,
+				"name": "hong",
+				"phone": "010-0000-0000",
+				"type": "lg",
+				"childnode": []
+			}
+		]
+	}]
+}]
