f8a4d444901813314b71aaae73bbc8fa2e55861d
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-09-29 20:28:12 +0900

    Add files via upload

--- obj(ex).js
+++ obj(ex).js
@@ -1,58 +1,37 @@
 let numbers = [{
-    "id": 1,
     "name": "Yong",
-    "phone": "010-0000-0000",
     "type": "sk",
     "childnode": [{
-        "id": 11,
         "name": "echo",
-        "phone": "010-0000-1111",
-        "type": "kt",
-        "childnode": [
-            {
-                "id": 116,
-                "name": "kim",
-                "phone": "444-111-0200",
-                "type": "kt",
-                "childnode": [{
-                    "id": 1168,
-                    "name": "hani",
-                    "phone": "010-222-0000",
-                    "type": "sk",
-                    "childnode": []
-                }]
-            },
-            {
-                "id": 117,
-                "name": "hong",
-                "phone": "010-0000-0000",
-                "type": "sk",
-                "childnode": []
-            }]
+        "type": "sk",
+        "childnode": [{
+            "name": "cong",
+            "type": "sk",
+            "childnode": []
+        }]
+    }]
+},
+{
+    "name": "hong",
+    "type": "sk",
+    "childnode": [{
+        "name": "mong",
+        "type": "sk",
+        "childnode": []
     }]
 }]
 
 let array = [];
-let inObj = numbers[0];
 
-
-let obj = Object;
-
-
-
-
-
-
-
-function findSk(inObj) {
-    for (let key in inObj) {
-        console.log(key)
-        console.log(inObj)
-        if (inObj[key] === "sk") array.push(inObj.name);
+function findSk(numbers) {
+    console.log(numbers)
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i]["type"] === "sk") {
+            array.push(numbers[i]["name"])
+        console.log(numbers[i]["childnode"])
+        }
     }
 }
-let repeated = findSk(inObj);
 
-
-
+findSk(numbers)
 console.log(array)
