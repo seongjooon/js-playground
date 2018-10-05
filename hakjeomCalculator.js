8045ebb2fc6a9a3b72ad03cb7e30c8d4513c5e6a
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-10-05 20:22:27 +0900

    Add files via upload

--- hakjeomCalculator.js
+++ hakjeomCalculator.js
@@ -2,7 +2,7 @@
 let data = [
     { 'name': '데이터베이스', 'grade': 'A+', 'credit': 2, 'major': false },
     { 'name': '딥러닝', 'grade': 'A', 'credit': 1, 'major': true },
-    { 'name': '데이터베이스', 'grade': 'A', 'credit': 3, 'major': false },
+    { 'name': '인공지능', 'grade': 'A', 'credit': 3, 'major': false },
     { 'name': '교양영어', 'grade': 'B+', 'credit': 2, 'major': true },
     { 'name': '철학', 'grade': 'B+', 'credit': 1, 'major': false }
 ];
