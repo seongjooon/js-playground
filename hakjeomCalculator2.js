60ed16355272909c4685e970152786c08974d612
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-10-06 10:22:16 +0900

    Add files via upload

--- hakjeomCalculator2.js
+++ hakjeomCalculator2.js
@@ -1,4 +1,4 @@
-//학점계산기
+// 학점계산기
 let data = [
     { 'name': '데이터베이스', 'grade': 'A+', 'credit': 2, 'major': false },
     { 'name': '딥러닝', 'grade': 'A', 'credit': 1, 'major': true },
@@ -8,7 +8,7 @@ let data = [
 ];
 // 학점 기준에 따라 변경
 let gradeTable = { 'A+': 4.5, 'A': 4, 'B+': 3.5, 'B': 3, 'C+': 2.5, 'C': 2, 'D+': 1.5, 'D': 1, 'F': 0 }
-//전체 이수학점 하나씩 받은 배열
+// 전체점수 배열(등급에 따라 점수 상환)
 function gradePush(data, gradeTable) {
     let wholeGrade = [];
     for (i = 0; i < data.length; i++) {
@@ -19,7 +19,7 @@ function gradePush(data, gradeTable) {
 }
 let wholeGrade = gradePush(data, gradeTable)
 console.log(gradePush(data, gradeTable))
-// 전공학점 하나씩 받은 배열
+// 전공점수 배열
 function majorGradePush(data, gradeTable) {
     let majorGrade = [];
     for (i = 0; i < data.length; i++) {
@@ -30,7 +30,7 @@ function majorGradePush(data, gradeTable) {
 }
 let majorGrade = majorGradePush(data, gradeTable)
 console.log(majorGradePush(data, gradeTable))
-//이수학점 배열
+// 이수학점 배열
 function creditPush(data) {
     let creditArray = [];
     for (i = 0; i < data.length; i++) {
@@ -41,7 +41,7 @@ function creditPush(data) {
 }
 let creditArray = creditPush(data);
 console.log(creditArray)
-//전공이수학점 배열
+// 전공이수학점 배열
 function majorCreditPush(data) {
     let majorCreditArray = [];
     for (i = 0; i < data.length; i++) {
@@ -52,7 +52,7 @@ function majorCreditPush(data) {
 }
 let majorCreditArray = majorCreditPush(data);
 console.log(majorCreditArray)
-// 평균값 구해주는 함수
+// 평균값 함수
 function gradeAverageCalculator(numberArray) {
     let numberSum = numberArray.reduce((acc, cur, i) => {
         return acc + cur;
@@ -71,7 +71,7 @@ function creditSum(numberArray) {
     return numberSum
 }
 
-//main 함수
+// main 함수
 function showGrade() {
     let parameter = arguments;
     let wholeGradeAverage = gradeAverageCalculator(wholeGrade);
